a9bc8008dbc3ada96cf4f36f93a10a59cc327785
Miguel Enrique Chowell Rangel <mechowell@gmail.com>
2016-09-02 21:50:38 -0500

    Initial Commit

---
+++ Remote2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Remote2
+//
+//  Created by Miguel Enrique Chowell Rangel on 02/09/16.
+//  Copyright © 2016 Miguel Enrique Chowell Rangel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
